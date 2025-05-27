bd2aa86efd23e2d0569e34d3fc634323bc087754
allbrann <b.t.marine@outlook.com>
2025-05-27 15:11:28 +1000

    🔥 First full site commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/hazeharvest/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+})
